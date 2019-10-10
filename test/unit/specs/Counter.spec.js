e668239d75ca4a56f00d58734569ec7fbf2811d7
zhangyuhui <842403078@qq.com>
2019-10-10 19:46:07 +0800

    使用vue-test-utils测试用例

--- test/unit/specs/Counter.spec.js
+++ test/unit/specs/Counter.spec.js
@@ -1,5 +1,33 @@
 import Vue from 'vue'
 import Counter from '@/views/Counter'
+import { mount } from 'vue-test-utils'
+
+// chai断言库
+// 相等或不相等
+// expect(1 + 1).to.be.equal(2);
+// expect(1 + 1).to.be.not.equal(3);
+
+// // 布尔值为true
+// expect('hello').to.be.ok;
+// expect(false).to.not.be.ok;
+
+// // typeof
+// expect('test').to.be.a('string');
+// expect({ foo: 'bar' }).to.be.an('object');
+// expect(foo).to.be.an.instanceof(Foo);
+
+// // include
+// expect([1,2,3]).to.include(2);
+// expect('foobar').to.contain('foo');
+// expect({ foo: 'bar', hello: 'universe' }).to.include.keys('foo');
+
+// // empty
+// expect([]).to.be.empty;
+// expect('').to.be.empty;
+// expect({}).to.be.empty;
+
+// // match
+// expect('foobar').to.match(/^foo/);
 
 describe('counter.vue', () => {
   it('点击按钮之后，count的值应该为1', () => {
@@ -31,6 +59,11 @@ describe('counter.vue', () => {
       expect(Number(vm.$el.querySelector('.num').textContent)).to.equal(1)
       done()
     }, 1000)
+  })
+
+  it('has a button', () => {
+    const wrapper = mount(Counter)
+    expect(wrapper.contains('button')).to.be.ok
   })
 })
 
